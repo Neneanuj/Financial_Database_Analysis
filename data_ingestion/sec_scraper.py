665babb5921d2b7db24f0c40a4cffca699431cad
YUNGROUKO <ko.yun@northeastern.edu>
2025-02-13 22:03:31 -0500

    json pipeline

--- data_ingestion/sec_scraper.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = "https://www.sec.gov/data-research/sec-markets-data/financial-statement-data-sets"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-
-dataset_links = [link['href'] for link in soup.find_all('a', href=True) if 'financial-statement' in link['href']]
-print(dataset_links)
